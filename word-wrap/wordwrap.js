8333d41c02e2d9c56dbec4e265739f1d224b8535
Timothy Williams <7538854+tim-br@users.noreply.github.com>
2025-01-18 19:40:44 -0500

    init commit

---
+++ word-wrap/wordwrap.js
@@ -0,0 +1,40 @@
+function wordWrapHelper(acc, words, maxChars){
+  if(words.length < maxChars){
+    acc.push(words)
+    return acc
+  } else {
+    let xWords = words.split(' ')
+    let counter = 0
+    let line = ''
+    while(counter < maxChars && xWords.length) {
+      let firstWord = xWords[0]  
+      let nextWord = xWords[1]
+      if(!(counter + firstWord.length > maxChars)){
+	xWords.shift()
+        counter += firstWord.length
+        line += firstWord
+	if(nextWord.length <= maxChars - counter){
+          line += ' '
+	  counter += 1
+	} 
+      } else {
+        break
+      }
+       
+    }
+    acc.push(line)
+    let joined = xWords.join(' ')
+    return wordWrapHelper(acc, joined, maxChars)
+  }
+}
+
+function wordWrap(words, maxChars) {
+  return wordWrapHelper([], words, maxChars)
+}
+
+
+console.log(wordWrap("This is a test of the word wrap", 6));
+console.log(wordWrap("This is a test of", 10));
+console.log(wordWrap("This is a test of the word", 10));
+console.log(wordWrap("This is a test of the word wrap function", 10));
+
---
+++ word-wrap/wordwrap_reduce.js
@@ -0,0 +1,45 @@
+function wordWrap(words, maxChars) {
+  let arr = words.split(' ')
+  const res = arr.reduce(
+    (acc, currentValue) => {
+       let currentCountX = acc.currentCount
+       if(currentCountX >= maxChars) {
+         currentCountX = 0
+       }
+       if(currentCountX < 1){
+         acc.arr.push('')
+       }
+       let space = currentCountX === 0 ? '' : ' '
+       let indent = currentCountX === 0 ? 0 : 1 
+
+       if(currentValue.length + currentCountX + indent <= maxChars ) {
+	 let lastItem = acc.arr.pop()
+	 lastItem += space  + currentValue
+	 acc.arr.push(lastItem)
+	 currentCountX += currentValue.length + indent 
+         return {arr: acc.arr, currentCount: currentCountX, lastWord: null }
+       } else {
+	  currentCountX = 0
+          space = currentCountX === 0 ? '' : ' '
+          indent = currentCountX === 0 ? 0 : 1 
+
+          if(currentValue && (currentValue.length + indent  <= maxChars)) {
+	     acc.arr.push(space + currentValue)
+	     currentCountX += currentValue.length + indent 
+          }
+         return {arr: acc.arr, currentCount: currentCountX, lastWord: currentValue}
+       }
+    },
+    {arr: [], currentCount: 0, lastWord: null} 
+  )
+
+  return res.arr
+}
+
+
+console.log(wordWrap("This is a test of the word wrap", 6));
+console.log(wordWrap("This is a test of", 10));
+console.log(wordWrap("This is a test of the word", 10));
+console.log(wordWrap("This is a test of the word wrap function", 10));
+
+
