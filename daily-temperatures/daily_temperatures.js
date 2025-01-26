3c93a6c5088180c29ff076fe9a80486ca9ef05e7
Timothy Williams <7538854+tim-br@users.noreply.github.com>
2025-01-26 10:16:55 -0500

    daily temperatures

---
+++ daily-temperatures/daily_temperatures.js
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/daily-temperatures/description/?envType=problem-list-v2&envId=9rt1jt27
+
+function dailyTemperaturesHelper(temperatures, accumulator, result, pos) {
+  if (!temperatures.length) {
+    return result;
+  }
+
+  let first = temperatures.shift();
+
+  const nextAccumulator = accumulator
+    .filter((x) => first <= x.temp)
+    .map((x) => ({ ...x, numOfDays: x.numOfDays + 1 }));
+  const firstTempWarmerArr = accumulator.filter((x) => first > x.temp);
+
+  firstTempWarmerArr.map((x) => (result[x.pos] = x.numOfDays + 1));
+  nextAccumulator.push({ temp: first, numOfDays: 0, pos });
+
+  return dailyTemperaturesHelper(
+    temperatures,
+    nextAccumulator,
+    result,
+    pos + 1,
+  );
+}
+
+var dailyTemperatures = function (temperatures) {
+  return dailyTemperaturesHelper(
+    temperatures,
+    [],
+    Array(temperatures.length).fill(0),
+    0,
+  );
+};
+
+//const assert = require("assert");
+import { deepEqual } from "assert";
+
+deepEqual(
+  dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]),
+  [1, 1, 4, 2, 1, 1, 0, 0],
+);
+deepEqual(dailyTemperatures([30, 40, 50, 60]), [1, 1, 1, 0]);
+deepEqual(dailyTemperatures([30, 60, 90]), [1, 1, 0]);
